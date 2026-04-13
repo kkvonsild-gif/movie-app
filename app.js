0d25eb6d0f26bca46e559a71ca04113b78475917
kkvonsild-gif <kkvonsild@gmail.com>
2026-04-13 17:52:00 +0200

    DAG 2 - færdig med Opgave 2 (arrays og loops)
    :)

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
 "use strict";
 
-console.log("App starter...");
+console.log("Movie App - DAG 2 starter...");
 
-// Finder mine knapper
-const clickBtn = document.querySelector("#click-button");
-const countDisplay = document.querySelector("#counter");
-const resetBtn = document.querySelector("#reset-button");
 
-//Tester at de er fundet
-console.log(clickBtn);
-console.log(countDisplay);
-console.log(resetBtn);
+const movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight", "Pulp Fiction", "Lord of the Rings", "The Revenant"];
 
-// Laver tæller-variablen
-let count = 0;
+console.log("Antal film:", movies.length);
 
-// Eventlistener der får klik til at tælle op
-clickBtn.addEventListener("click", function() {
-    count = count + 1;
-    countDisplay.textContent = count;
-    console.log(count);
-    if (count === 10) {
-        alert("Du har klikket 10 gange");
-    }
-})
 
-// Eventlistener til nulstille-knappen
-resetBtn.addEventListener("click", function() {
-    count = 0;
-    countDisplay.textContent = count;
-    console.log(count);
-    if (count = 0) {countDisplay.style.color = "lightgreen";
-        
-    }
-})
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
 
-// Tæl ned
-const minusBtn = document.querySelector("#minus-button");
-console.log(minusBtn);
+for (const movie of movies) {
+  const html =  `
+    <article class="movie-card">
+      <div class="movie-info">
+        <h3>${movie}</h3>
+      </div>
+    </article>
+  `;
 
-minusBtn.addEventListener("click", function() {
-    count = count - 1;
-    countDisplay.textContent = count;
-    console.log(count);
-
-})
-
+  movieList.insertAdjacentHTML("beforeend", html);
+}
