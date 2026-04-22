71526e02d2a8857fc2eb5718e7ea96dcbbf13c13
kkvonsild-gif <kkvonsild@gmail.com>
2026-04-22 09:45:13 +0200

    commit

--- app.js
+++ app.js
@@ -7,22 +7,26 @@ const MOVIES_URL =
 let allMovies = [];
 const movieList = document.querySelector("#movie-list");
 const genreSelect = document.querySelector("#genre-select");
+const searchInput = document.querySelector("#search-input");
 const movieCount = document.querySelector("#movie-count");
 
 fetchMovies();
 
-async function fetchMovies() {
+async function fetchMovies() {  
   const response = await fetch(MOVIES_URL);
   allMovies = await response.json();
 
   populateGenreSelect();
   showMovies(allMovies);
 
-  genreSelect.addEventListener("change", applyGenreFilter);
+  genreSelect.addEventListener("change", applyFilters);
+  searchInput.addEventListener("input", applyFilters);
 }
 
+
+// FILTRERING AF GENRER
 function populateGenreSelect() {
-  const genres = new Set();
+  const genres = new Set(); // Set er en datatype hvor der kun kan være én af hver slags
 
   for (const movie of allMovies) {
     for (const genre of movie.genre) {
@@ -40,19 +44,22 @@ function populateGenreSelect() {
   }
 }
 
-function applyGenreFilter() {
+
+function applyFilters() {
   const selectedGenre = genreSelect.value;
-  
-  if (selectedGenre === "all") {
-    showMovies(allMovies);
-    return;
-  }
+  const searchValue = searchInput.value.trim().toLowerCase();
+
   const filteredMovies = allMovies.filter(function (movie) {
-    return movie.genre.includes(selectedGenre);
+    const matchesGenre =
+      selectedGenre === "all" || movie.genre.includes(selectedGenre);
+    const matchesSearch = movie.title.toLowerCase().includes(searchValue);
+
+    return matchesGenre && matchesSearch;
   });
 
   showMovies(filteredMovies);
 }
+
 
 function showMovies(movies) {
   movieList.innerHTML = "";
@@ -60,7 +67,7 @@ function showMovies(movies) {
   for (const movie of movies) {
     showMovie(movie);
   }
-  
+
   movieCount.textContent = `Viser ${movies.length} film`;
 };
 
@@ -81,5 +88,3 @@ function showMovie(movie) {
 function formatMovieTitle(title, year) {
   return `${title} (${year})`;
 };
-
-console.log(formatMovieTitle("Inception", 2010));
