060611d08e01dfb46cafa3d74681cd5b6cf3c7c3
kkvonsild-gif <kkvonsild@gmail.com>
2026-04-22 08:28:40 +0200

    220426
    :)

--- app.js
+++ app.js
@@ -1,72 +1,85 @@
 "use strict";
 
-const movies = [
-  {
-    title: "Inception",
-    year: 2010,
-    rating: 8.8,
-    image:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
-  },
-  {
-    title: "The Matrix",
-    year: 1999,
-    rating: 8.7,
-    image: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg",
-  },
-  {
-    title: "The Dark Knight",
-    year: 2008,
-    rating: 9.0,
-    image:
-      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
-  },
-  {
-    title: "The Shawshank Redemption",
-    year: 1994,
-    rating: 9.3,
-    image: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
-  },
-  {
-    title: "Oppenheimer",
-    year: 2023,
-    rating: 8.4,
-    image:
-      "https://m.media-amazon.com/images/M/MV5BN2JkMDc5MGQtZjg3YS00NmFiLWIyZmQtZTJmNTM5MjVmYTQ4XkEyXkFqcGc@._V1_.jpg",
-  },
-  {
-    title: "Dune",
-    year: 2021,
-    rating: 8.0,
-    image:
-      "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
-  },
-];
+console.log("Movie App starter...");
 
+const MOVIES_URL =
+  "https://raw.githubusercontent.com/cederdorff/race/refs/heads/master/data/movies.json";
+let allMovies = [];
 const movieList = document.querySelector("#movie-list");
+const genreSelect = document.querySelector("#genre-select");
+const movieCount = document.querySelector("#movie-count");
 
-showMovies();
+fetchMovies();
 
-function showMovies() {
+async function fetchMovies() {
+  const response = await fetch(MOVIES_URL);
+  allMovies = await response.json();
+
+  populateGenreSelect();
+  showMovies(allMovies);
+
+  genreSelect.addEventListener("change", applyGenreFilter);
+}
+
+function populateGenreSelect() {
+  const genres = new Set();
+
+  for (const movie of allMovies) {
+    for (const genre of movie.genre) {
+      genres.add(genre);
+    }
+  }
+
+  const sortedGenres = [...genres].sort((a, b) => a.localeCompare(b));
+
+  for (const genre of sortedGenres) {
+    genreSelect.insertAdjacentHTML(
+      "beforeend",
+      `<option value="${genre}">${genre}</option>`,
+    );
+  }
+}
+
+function applyGenreFilter() {
+  const selectedGenre = genreSelect.value;
+  
+  if (selectedGenre === "all") {
+    showMovies(allMovies);
+    return;
+  }
+  const filteredMovies = allMovies.filter(function (movie) {
+    return movie.genre.includes(selectedGenre);
+  });
+
+  showMovies(filteredMovies);
+}
+
+function showMovies(movies) {
   movieList.innerHTML = "";
 
   for (const movie of movies) {
     showMovie(movie);
   }
-}
+  
+  movieCount.textContent = `Viser ${movies.length} film`;
+};
 
 function showMovie(movie) {
   const html = /* html */ `
     <article class="movie-card">
       <img class="movie-image" src="${movie.image}" alt="${movie.title}">
       <div class="movie-info">
-        <h3>${movie.title}</h3>
-        <p>År: ${movie.year}</p>
-        <p>Rating: ${movie.rating}</p>
+      <h3>${formatMovieTitle(movie.title, movie.year)}</h3>
+      <p>Rating: ${movie.rating}</p>
       </div>
     </article>
   `;
 
   movieList.insertAdjacentHTML("beforeend", html);
-}
+};
 
+function formatMovieTitle(title, year) {
+  return `${title} (${year})`;
+};
+
+console.log(formatMovieTitle("Inception", 2010));
